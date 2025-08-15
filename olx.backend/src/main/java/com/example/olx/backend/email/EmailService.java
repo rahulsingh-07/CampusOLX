81a2d5f7a99e19af4959a90672a31ded2c63bb67
rahulsingh-07 <bt22cse034@nituk.ac.in>
2025-08-15 03:31:06 +0000

    last

--- olx.backend/src/main/java/com/example/olx/backend/email/EmailService.java
+++ olx.backend/src/main/java/com/example/olx/backend/email/EmailService.java
@@ -2,7 +2,6 @@ package com.example.olx.backend.email;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
